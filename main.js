be179317f9b722fa24385538c6e394aa5cd01532
jay <jayrajshakha01@gmail.com>
2023-07-26 17:51:26 +0530

    All Done

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
 
-console.log('--------------------------------)');
-let c = rgba(109, 109, 225, 0.689);
+// HTML elements thats initalized and store in variables
+const hourElement = document.querySelector("#hrs");
+const minuteElement = document.querySelector("#min");
+const secondElement = document.querySelector("#sec");
+
+
+
+
+// set internal that repeatedly updates the current time and all elements values after 1 second 
+setInterval(() => {
+
+    // create a timer
+    let currentTime = new Date();
+
+    
+    // whole current time converted to hour
+    let hour = currentTime.getHours();
+    if(hour < 10){
+        hourElement.textContent = "0" + hour;
+    }
+    else{
+        hourElement.textContent = hour;
+    }
+    
+     // whole current time converted to mi
+    let minute = currentTime.getMinutes();
+    if(minute < 10){
+        minuteElement.textContent = "0" + minute;
+    }
+    else{
+        minuteElement.textContent = minute;
+    }
+ 
+     // whole current time converted to seconds
+    let second = currentTime.getSeconds();
+    if(second < 10){
+        secondElement.textContent = "0" + second;
+    }
+    else{
+        secondElement.textContent = second;
+    }
+    
+}, 1000);
